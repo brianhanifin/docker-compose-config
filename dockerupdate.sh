09de041c2c35149352a001c72df62825c458df3f
brianhanifin <code@brianhanifin.com>
2019-03-26 21:13:56 -0700

    dockerupdate: noprompt; returning to unmodified ha

--- dockerupdate.sh
+++ dockerupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt
 sudo docker-compose stop $1 $2 $3
-sudo docker system prune --all --volumes
+sudo docker system prune --force --all --volumes
 #sudo docker-compose pull $1 $2 $3
 sudo docker-compose up -d $1 $2 $3
